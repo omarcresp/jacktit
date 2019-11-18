5d3855ffd6b91e01bb0c643e149f91a2b0ec119c
omar <crespomerchano@gmail.com>
2019-11-18 00:21:14 -0500

    add button atom

---
+++ src/components/atoms/buttons.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+interface IJackButtonProps {
+  size?: number;
+  color?: string;
+}
+
+export const JackButton = styled.button<IJackButtonProps>`
+  background-color: var(--${props => props.color || 'primary'});
+  color: var(--text-${props => props.color || 'primary'});
+  border: none;
+  border-radius: 3px;
+  padding: 5px 20px;
+  cursor: pointer;
+
+  :focus {
+    outline: none; 
+  }
+
+  :active {
+    background: var(--${props => props.color || 'primary'})
+      linear-gradient(
+        rgba(255, 255, 255, 0.1),
+        rgba(255, 255, 255, 0.1)
+      );
+  }
+`;
