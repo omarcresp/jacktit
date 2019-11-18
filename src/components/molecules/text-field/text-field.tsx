79172c6ad8db2f741abce56b61d084f8b5b1456b
omar <crespomerchano@gmail.com>
2019-11-18 00:44:56 -0500

    add text field molecule

---
+++ src/components/molecules/text-field/text-field.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { JackInput } from '../../atoms/input';
+import { JackLabel } from '../../atoms/label';
+
+import './text-field.css';
+
+interface IJackTextFieldProps {
+  label: string;
+  handleChange?: any;
+  handleBlur?: any;
+  value: string;
+  name: string;
+}
+
+export const JackTextField = (props: IJackTextFieldProps) => {
+  const {
+    handleBlur,
+    handleChange,
+    value,
+    name,
+    label
+  } = props;
+
+  return (
+    <div className="textfield-container">
+      <JackInput className="textfield-input"
+        name={name}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={value}
+      ></JackInput>
+      <JackLabel className="textfield-label">{ label }</JackLabel>
+    </div>
+  );
+};
